d1149ffbbbeea0d9f9b312e64a81b24a622e0e14
KOSW32 <119171476+KOSW32@users.noreply.github.com>
2026-02-15 00:35:59 +0200

    Add files via upload

---
+++ Делегати та події/Делегати та події/CreditCard.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public class CreditCard
+{
+    public string CardNumber { get; }
+    public string OwnerName { get; }
+    public string ExpiryDate { get; }
+    public int Pin { get; private set; }
+    public double CreditLimit { get; }
+    public double Balance { get; private set; }
+
+
+    public event Action<string>? OnDeposit;
+    public event Action<string>? OnWithdraw;
+    public event Action<string>? OnCreditStart;
+    public event Action<string>? OnLimitReached;
+    public event Action<string>? OnPinChanged;
+
+    public CreditCard(string number, string owner, string expiry,
+                      int pin, double limit, double balance)
+    {
+        CardNumber = number;
+        OwnerName = owner;
+        ExpiryDate = expiry;
+        Pin = pin;
+        CreditLimit = limit;
+        Balance = balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+        OnDeposit?.Invoke($"Рахунок поповнено на {amount}. Баланс: {Balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        Balance -= amount;
+        OnWithdraw?.Invoke($"Витрачено {amount}. Баланс: {Balance}");
+
+        if (Balance < 0)
+            OnCreditStart?.Invoke("Почалося використання кредитних коштів");
+
+        if (Math.Abs(Balance) >= CreditLimit)
+            OnLimitReached?.Invoke("Досягнуто кредитного ліміту!");
+    }
+
+    public void ChangePin(int newPin)
+    {
+        Pin = newPin;
+        OnPinChanged?.Invoke("PIN успішно змінено");
+    }
+}
---
+++ Делегати та події/Делегати та події/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Action showTime = () =>
+        {
+            Console.WriteLine("Поточний час: " + DateTime.Now.ToLongTimeString());
+        };
+
+        Action showDate = () =>
+        {
+            Console.WriteLine("Поточна дата: " + DateTime.Now.ToShortDateString());
+        };
+
+        Action showDay = () =>
+        {
+            Console.WriteLine("День тижня: " + DateTime.Now.DayOfWeek);
+        };
+
+      
+        Func<double, double, double> triangleArea = (a, h) =>
+        {
+            return 0.5 * a * h;
+        };
+
+        Func<double, double, double> rectangleArea = (a, b) =>
+        {
+            return a * b;
+        };
+
+        Predicate<double> isPositive = x => x > 0;
+
+        showTime();
+        showDate();
+        showDay();
+
+        Console.WriteLine("Площа трикутника: " + triangleArea(10, 5));
+        Console.WriteLine("Площа прямокутника: " + rectangleArea(4, 6));
+
+        Console.WriteLine("Число додатне? " + isPositive(5));
+
+        Console.WriteLine("\n--- Кредитна картка ---\n");
+
+        CreditCard card = new CreditCard(
+            "1234 5678 9012 3456",
+            "Іваненко Іван",
+            "12/26",
+            1234,
+            5000,
+            1000
+        );
+
+     
+        card.OnDeposit += msg => Console.WriteLine(msg);
+        card.OnWithdraw += msg => Console.WriteLine(msg);
+        card.OnCreditStart += msg => Console.WriteLine(msg);
+        card.OnLimitReached += msg => Console.WriteLine(msg);
+        card.OnPinChanged += msg => Console.WriteLine(msg);
+
+        card.Deposit(500);
+        card.Withdraw(2000);
+        card.ChangePin(4321);
+    }
+}
