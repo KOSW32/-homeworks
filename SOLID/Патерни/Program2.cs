2f96ceb3a678017f44b790b57b4ac1ca323925e4
KOSW32 <119171476+KOSW32@users.noreply.github.com>
2026-02-23 22:26:03 +0200

    Add files via upload

---
+++ SOLID/Патерни/Program2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AdapterExample
+{
+
+    public interface ICoordinatesService
+    {
+        (double Latitude, double Longitude) GetCoordinates();
+    }
+
+ 
+    public class GeoLocation
+    {
+        public string GetCoordinates()
+        {
+            return "37.7749, -122.4194";
+        }
+    }
+
+    public class GeoLocationAdapter : ICoordinatesService
+    {
+        private GeoLocation _geoLocation;
+
+        public GeoLocationAdapter(GeoLocation geoLocation)
+        {
+            _geoLocation = geoLocation;
+        }
+
+        public (double Latitude, double Longitude) GetCoordinates()
+        {
+            string coordinates = _geoLocation.GetCoordinates();
+
+            string[] parts = coordinates.Split(',');
+
+            double latitude = double.Parse(parts[0]);
+            double longitude = double.Parse(parts[1]);
+
+            return (latitude, longitude);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeoLocation geoLocation = new GeoLocation();
+
+            ICoordinatesService coordinatesService =
+                new GeoLocationAdapter(geoLocation);
+
+            var coordinates = coordinatesService.GetCoordinates();
+
+            Console.WriteLine(
+                $"Latitude: {coordinates.Latitude}, Longitude: {coordinates.Longitude}");
+        }
+    }
+}
